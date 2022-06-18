7bb5722f8208b284948e7508adaae0a152887b37
jumalaw98 <jumalawrence98@gmail.com>
2022-06-18 10:44:09 +0300

    solution_Lawrence_Juma: cohort 4

--- src/index.php
+++ src/index.php
@@ -3,7 +3,12 @@
 function prime($input)
 {
   // code goes here
+  for ($i = 2; $i <= $input - 1; $i++) {
+    if ($input % $i == 0) {
+        $value = True;
+    }
 }
+// if (isset($value) && $value)
 
 function solution($input)
 {
@@ -16,3 +21,4 @@ if (!count(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)))
   $input = $argv[1];
   var_dump(solution($input));
 }
+
